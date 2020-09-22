b98c6fc466bc6d9bc0031c4cfeb8a1582634903e
Hanbyeol Jo <hanbyeul04@kakao.com>
2020-09-22 21:54:52 +0900

    commit

--- index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var translator_1 = require("./src/translator");
-Object.defineProperty(exports, "Translator", { enumerable: true, get: function () { return translator_1.Translator; } });
